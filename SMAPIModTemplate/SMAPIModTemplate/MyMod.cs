72d8637a99d90c46c9eb47756bb2fe37f300ebc4
Tyler Staples <tstaples@akimbocreations.com>
2017-01-09 15:15:52 -0800

    Added the original project the template is based on. Moved generated template files into own folder.

---
+++ SMAPIModTemplate/SMAPIModTemplate/MyMod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StardewModdingAPI;
+
+namespace SMAPIModTemplate
+{
+    public class MyMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+        }
+    }
+}
