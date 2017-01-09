3e4e66efaa4ac7aef4e18235db1ec793adcf0ae3
Tyler Staples <tstaples@akimbocreations.com>
2017-01-09 15:10:53 -0800

    Created basic template.

---
+++ MyMod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StardewModdingAPI;
+
+namespace $safeprojectname$
+{
+    public class MyMod : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+        }
+    }
+}
